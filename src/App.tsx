2e8cdeef94568dff54100185600dd47acf32d05b
Gabriel Felix <gabrielfelxx@gmail.com>
2024-01-31 00:47:18 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <h1>GH PROFILE FINDER</h1>
+        <img src="#" alt="profile-img" />
+        <span>John Doe</span>
+      </div>
+    </>
+  )
+}
+
+export default App
